2f052b241919e0a8847afef230f16c1bb30edfda
fandu121 <117754165+fandu121@users.noreply.github.com>
2023-02-03 20:36:46 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
-
+const ourGroup ={fisrtmember: {    name: "Anamaria",    surname:"Sulikashvili",    age :19,},secondmember:{    name:"Shota",    Surname:"Alania",    age:24,},thirdmember:{    name:"Aleqsandre",    surname:"Afaqidze",    age:20,}
+};console.log(ourGroup);
  drinks = [
     { name:"cola", price: 25 },
     { name:"pepsi", price: 20 },
